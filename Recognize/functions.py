22c4bc721c2f5e82e96a2e72f15087f1320b62b7
gyuwon <licmm0520@gmail.com>
2023-10-15 23:47:06 +0900

    매핑 완료 by 준형님

--- Recognize/functions.py
+++ Recognize/functions.py
@@ -15,3 +15,12 @@ def closing(image):
     kernel = np.ones((weighted(5), weighted(5)), np.uint8)
     image = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel)
     return image
+
+def get_number(note):
+    mapping = {
+        'C4': '3/5', 'D4': '5/5', 'E4': '7/5', 'F4': '8/5', 'G4': '10/5',
+        'A4': '12/5', 'B4': '14/5', 'C5': '15/5', 'D5': '17/5', 'E5': '19/5',
+        'F5': '20/5'
+    }
+
+    return mapping.get(note, "해당 문자열에 대한 숫자가 없습니다.")

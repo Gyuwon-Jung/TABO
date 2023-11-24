46f793e35886d8cae3ec6f8656cb8b8e46cdb165
gyuwon <licmm0520@gmail.com>
2023-11-25 00:43:26 +0900

    카메라쪽 코드 수정

--- Recognize/camera.py
+++ Recognize/camera.py
@@ -1,12 +1,13 @@
-#기울어진 오선에 대한 보정 test.py
+#기울어진 오선에 대한 보정 camera.py
 import os
 import numpy as np
 import cv2
 import functions as fs
+import modules
 
 # 이미지를 읽어옵니다.
 resource_path = os.getcwd() + "/resources/"
-src = cv2.imread(resource_path+"rotate.jpg")
+src = cv2.imread(resource_path+"music.jpg")
 dst = src.copy()
 gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
 length = src.shape[1]
@@ -21,7 +22,6 @@ for line in lines:
     if(angle>=0): # 직선 무시 일반적인 악보는 기울기가 0.0이기때문
         angles.append(angle)
 avg_angle = np.mean(angles)
-print(angles)
 
 # 이미지의 중심을 기준으로 회전 변환 행렬을 계산합니다.
 h, w = src.shape[:2]
@@ -29,15 +29,10 @@ center = (w / 2, h / 2)
 M = cv2.getRotationMatrix2D(center, avg_angle, 1)
 rotated = cv2.warpAffine(src, M, (w, h))
 
-# 회전된 이미지의 뒷 배경을 흰색으로 채우면서 일부 영역을 잘라냅니다.
-rotated_with_white_bg = np.full_like(rotated, (255, 255, 255), dtype=np.uint8)
-rotated_with_white_bg[h//2 - src.shape[0]//2: h//2 + src.shape[0]//2, w//2 - src.shape[1]//2: w//2 + src.shape[1]//2] = rotated
+image_0 = fs.camera_threshold(rotated)
 
-image = fs.camera_threshold(rotated_with_white_bg)
 
 # 결과를 출력합니다.
-cv2.imshow("Original", src)
-cv2.imshow("Rotated Image", rotated_with_white_bg)
-cv2.imshow("binarization", image)
+cv2.imshow('result', image_0)
 cv2.waitKey(0)
 cv2.destroyAllWindows()

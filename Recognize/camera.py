86d2c61eb0b598bfcfb9ecccde44848d17a33077
gyuwon <licmm0520@gmail.com>
2023-11-25 09:43:19 +0900

    카메라쪽 코드 수정

--- Recognize/camera.py
+++ Recognize/camera.py
@@ -7,7 +7,7 @@ import modules
 
 # 이미지를 읽어옵니다.
 resource_path = os.getcwd() + "/resources/"
-src = cv2.imread(resource_path+"music.jpg")
+src = cv2.imread(resource_path+"rotate.jpg")
 dst = src.copy()
 gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
 length = src.shape[1]
@@ -19,7 +19,7 @@ angles = []
 for line in lines:
     line = line[0]
     angle = np.arctan2(line[3] - line[1], line[2] - line[0]) * 180. / np.pi
-    if(angle>=0): # 직선 무시 일반적인 악보는 기울기가 0.0이기때문
+    if(angle>=0): # 세로 직선 무시 일반적인 악보는 기울기가 0.0이기때문
         angles.append(angle)
 avg_angle = np.mean(angles)
 
@@ -28,11 +28,17 @@ h, w = src.shape[:2]
 center = (w / 2, h / 2)
 M = cv2.getRotationMatrix2D(center, avg_angle, 1)
 rotated = cv2.warpAffine(src, M, (w, h))
+#rotated = cv2.warpAffine(src, M, (w, h), borderMode=cv2.BORDER_CONSTANT, borderValue=(255,255,255)) #회전 이후 검은색 부분을 흰색으로 칠해주는 함수 지금은 threshold만 하면 해결
 
-image_0 = fs.camera_threshold(rotated)
+image = fs.camera_threshold(rotated)
 
+cnt, labels, stats, centroids = cv2.connectedComponentsWithStats(image)  # 레이블링
+for i in range(1, cnt):
+    x, y, w, h, area = stats[i]
+    if w > image.shape[1] * 0.75:  # 보표 영역에만
+        cv2.rectangle(image, (x, y, w, h), (255, 0, 0), 1)  # 사각형 그리기
 
 # 결과를 출력합니다.
-cv2.imshow('result', image_0)
+cv2.imshow('result', image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
